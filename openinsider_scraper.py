b9e60b9e284ac478ceda6b407515eb56a776fbbc
MacKonMiNI <makon324@gmail.com>
2024-10-28 23:52:50 +0100

    added .txt, .xlsx and .db file support

--- openinsider_scraper.py
+++ openinsider_scraper.py
@@ -10,6 +10,7 @@ import pandas as pd
 import math
 import multiprocessing as mp
 from pathlib import Path
+import sqlite3
 
 columns = [  # dataframe column names
     'X', 'Filing Date', 'Trade Date', 'Ticker', 'Insider Name', 'Title', 'Trade Type',
@@ -22,13 +23,17 @@ script_dir = Path(__file__).resolve().parent  # path to parent folder of the scr
 
 ## CONST VARIABLES, CHANGE FREELY ##
 
-link = script_dir / "scrapes.csv"  # link to csv with scrapes
+link = script_dir / "scrapes.csv"  # link to file with scraped data - can be any of the following types: '.csv', '.txt', '.xlsx', '.db'
+
+csv_sep = ','  # separator in case link is .csv or .txt file
 
 default_ticker_link = script_dir / "default_tickers.txt"  # link to txt with default ticker list
 
 num_processes = 4  # number of processes
 
 #######
+
+
 
 
 def Strip_array(vec):  # strips every ticker in an array from white characters
@@ -54,8 +59,7 @@ def tickers_from_file(path):  # takes file path and outputs list of tickers in t
 def scrape_page(ticker):
     # openinsider url to data with specific ticker
     url = "http://openinsider.com/screener?s=" + ticker + \
-          "&o=&pl=&ph=&ll=&lh=&fd=0&fdr=&td=0&tdr=&fdlyl=&fdlyh=&daysago=&xp=1&xs=1&vl=&vh=&ocl=&och=&sic1=-1&sicl=100&sich=9999&grp=0&nfl=&nfh=&nil=&nih=&nol=&noh=&v2l=&v2h=&oc2l=&oc2h=&sortcol=1&cnt=1000&page=1"
-
+          "&o=&pl=&ph=&ll=&lh=&fd=0&fdr=&td=0&tdr=&fdlyl=&fdlyh=&daysago=&xp=1&xs=1&xa=1&xd=1&xg=1&xf=1&xm=1&xx=1&xc=1&xw=1&vl=&vh=&ocl=&och=&sic1=-1&sicl=100&sich=9999&grp=0&nfl=&nfh=&nil=&nih=&nol=&noh=&v2l=&v2h=&oc2l=&oc2h=&sortcol=0&cnt=1000&page=1"
     try:
         page = requests.get(url, timeout=10)
     except:
@@ -111,30 +115,47 @@ if __name__ == "__main__":
         tickers = sys.argv[1:]
 
 
-    while len(tickers) < 10 * num_processes and num_processes > 1:  # if the number of tickers is small use smaller number of threads
+    while len(tickers) < 20 * num_processes and num_processes > 1:  # if the number of tickers is small use smaller number of threads
         num_processes //= 2
 
     # read data already scraped in csv
     df = pd.DataFrame(columns = columns)
     try:
-        df = pd.read_csv(link, sep = ';', low_memory=False)
+        if(link.suffix in ['.csv', '.txt']):
+            df = pd.read_csv(link, sep = csv_sep, low_memory=False)
+        elif(link.suffix == '.xlsx'):
+            df = pd.read_excel(link, index_col=0)
+        elif(link.suffix == '.db'):
+            conn = sqlite3.connect(link)
+            df = pd.read_sql_query("SELECT * FROM people", conn)
+        else:
+            print("unsuported database file format")
+            exit()
+
     except:
         pass
+
 
     # scrape openinsider using multithreading and add resutls to df
     with mp.Pool(processes = num_processes) as pool:
         results = pool.imap_unordered(worker, split_into_sublists(tickers, num_processes))
 
         for result in results:
-            df = pd.concat([df, result], ignore_index=True)
+            df = pd.concat([df, result], ignore_index=False)
 
     # drop duplicates, there might have been data already in csv that we scraped once again
     df = df.drop_duplicates(subset = ['Filing Date', 'Trade Date', 'Ticker', 'Insider Name', 'Title', 'Trade Type',
         'Price', 'Qty', 'Owned', 'deltaOwn', 'Value'])
 
     # save and print data
-    df.to_csv(link, sep = ';', index=False)
+    if(link.suffix in ['.csv', '.txt']):
+        df.to_csv(link, sep = csv_sep, index=False)
+    elif(link.suffix == '.xlsx'):
+        df.to_excel(link)
+    else:  # if suffix == '.db'
+        conn = sqlite3.connect(link)
+        df.to_sql('people', conn, if_exists='replace', index=False)
+        conn.close()
     print(df)
-
 
 
